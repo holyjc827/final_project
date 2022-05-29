c76f5a4dbd7bf736926fbcb52c764dfe0084d110
holyjc827 <holyjc827@gmail.com>
2022-05-29 12:27:01 +0200

    Add generate search query function

--- main.py
+++ main.py
@@ -5,8 +5,17 @@ class SearchQueryGenerator:
         self.type = type
         self.document = document
 
-    def print_yes(self):
-        print(self.type)
+    def generate_search_query(self):
+        most_repeated_keywords = self.__get_most_repeated_keywords()
+        query_string = ""
+        for word in most_repeated_keywords:
+            if len(query_string) == 0:
+                query_string = '"' + word + '"'
+            else:
+                query_string += ' AND ' + '"' + word + '"'
+        return query_string
+
+    # Private methods 
 
     def __tokenize(self):
         sentences = self.document.split(".")
@@ -17,7 +26,7 @@ class SearchQueryGenerator:
         cleaned_tokens = [word for word in sum(tokens,[]) if word]
         return cleaned_tokens
     
-    def get_most_repeated_keywords(self):
+    def __get_most_repeated_keywords(self):
         tokens = self.__tokenize()
         token_dict = {}
         repeated_keywords = []
@@ -29,8 +38,8 @@ class SearchQueryGenerator:
                 token_dict.setdefault(token, 1)
         
         token_dict = dict(sorted(token_dict.items(), key=lambda item: item[1], reverse=True))
-        repeated_keywords= [k for k, v in dict(itertools.islice(token_dict.items(), 3)).items()]
-        print(repeated_keywords)
+        repeated_keywords = [k for k, v in dict(itertools.islice(token_dict.items(), 5)).items()]
+        return repeated_keywords
 
-app = SearchQueryGenerator("job","to me you are beautiful. I hope you are doing well.")
-app.get_most_repeated_keywords()
+app = SearchQueryGenerator("job", "to me you are beautiful. I hope you are doing well.")
+print(app.generate_search_query())
